fb985ceba86b17d36ae7081748371dd1631ac288
Luka <luka.ivanovic1@protonmail.com>
2026-02-02 00:41:09 +0100

    some changes

--- 3coloring/supervisor.c
+++ 3coloring/supervisor.c
@@ -5,21 +5,36 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <semaphore.h>
+#include <errno.h>
 #include "common.h"
 #include "supervisor.h"
 
-unsigned limit = 0;
-unsigned delay = 0;
-sem_t *semFree;
-sem_t *semUsed;
-sem_t *semGenerator;
 
 int main(int argc, char **argv) {
-    if (parseArgs(argc, argv) == EXIT_FAILURE) {
+
+    unsigned long  limit = 0;
+    unsigned long delay = 0;
+    int boolean = 0;
+    sems_t allSems = {
+        NULL,
+        NULL,
+        NULL
+    };
+
+    if (parseArgs(argc, argv, &limit, &delay) == EXIT_FAILURE) {
         return EXIT_FAILURE;
     }
-    if (setUpCircBuf() == EXIT_FAILURE) {
+    if (setUpCircBuf(&allSems) == EXIT_FAILURE) {
         return EXIT_FAILURE;
+    }
+    if (limit > 0) boolean = 1;
+    while (boolean || limit > 0) {
+        //read from shm
+        sem_wait(allSems.semUsed);
+
+
+        sem_post(allSems.semFree);
+        if (limit > 0) limit--;
     }
 
     return EXIT_SUCCESS;
@@ -29,20 +44,22 @@ void printUsage(void) {
     printf("supervisor [-n limit] [-w delay]");
 }
 
-int parseArgs(int argc, char **argv) {
+int parseArgs(int argc, char **argv, unsigned long *limit, unsigned long *delay) {
     int opt;
     while ((opt = getopt(argc, argv, "n:w:"))!= -1) {
         switch (opt) {
             case 'n':
-                limit = strtol(optarg, NULL, 10);
-                if (limit == LONG_MAX || limit == LONG_MIN) {
+                errno = 0;
+                *limit = (unsigned) strtol(optarg, NULL, 10);
+                if (errno != 0) {
                     perror("Invalid delay");
                     return EXIT_FAILURE;
                 }
                 break;
             case 'w':
-                delay = strtol(optarg, NULL, 10);
-                if (delay == LONG_MAX || delay == LONG_MIN) {
+                errno = 0;
+                *delay = (unsigned) strtol(optarg, NULL, 10);
+                if (errno != 0) {
                     perror("Invalid delay");
                     return EXIT_FAILURE;
                 }
@@ -55,37 +72,37 @@ int parseArgs(int argc, char **argv) {
     return EXIT_SUCCESS;
 }
 
-int setUpCircBuf(void) {
-    semFree = sem_open(SEM_NAME_FREE, O_CREAT |O_EXCL, SEM_MODE, MAX_CIRC_BUF_MEM);
-    if (semFree == SEM_FAILED) {
+int setUpCircBuf(sems_t *allSems) {
+    allSems->semFree = sem_open(SEM_NAME_FREE, O_CREAT | O_EXCL, SEM_MODE, MAX_CIRC_BUF_MEM);
+    if (allSems->semFree == SEM_FAILED) {
         perror("Failed to create semaphore");
         goto fail;
     }
 
-    semUsed = sem_open(SEM_NAME_USED, O_CREAT |O_EXCL, SEM_MODE, 0);
-    if (semUsed == SEM_FAILED) {
+    allSems->semUsed = sem_open(SEM_NAME_USED, O_CREAT |O_EXCL, SEM_MODE, 0);
+    if (allSems->semUsed == SEM_FAILED) {
         perror("Failed to create semaphore");
         goto fail;
     }
 
-    semGenerator = sem_open(SEM_NAME_GENERATOR, O_CREAT |O_EXCL, SEM_MODE, 1);
-    if (semGenerator == SEM_FAILED) {
+    allSems->semGenerator = sem_open(SEM_NAME_GENERATOR, O_CREAT |O_EXCL, SEM_MODE, 1);
+    if (allSems->semGenerator == SEM_FAILED) {
         perror("Failed to create semaphore");
         goto fail;
     }
     return EXIT_SUCCESS;
 
     fail:
-      if (semUsed != SEM_FAILED) {
-          sem_close(semUsed);
+      if (allSems->semUsed != SEM_FAILED) {
+          sem_close(allSems->semUsed);
           sem_unlink(SEM_NAME_USED);
       }
-      if (semGenerator != SEM_FAILED) {
-        sem_close(semGenerator);
+      if (allSems->semGenerator != SEM_FAILED) {
+        sem_close(allSems->semGenerator);
         sem_unlink(SEM_NAME_GENERATOR);
       }
-      if (semFree != SEM_FAILED) {
-        sem_close(semFree);
+      if (allSems->semFree != SEM_FAILED) {
+        sem_close(allSems->semFree);
         sem_unlink(SEM_NAME_FREE);
       }
       return EXIT_FAILURE;
--- 3coloring/supervisor.h
+++ 3coloring/supervisor.h
@@ -1,8 +1,14 @@
 #ifndef C_EXERCISES_SUPERVISOR_H
 #define C_EXERCISES_SUPERVISOR_H
 
+typedef struct {
+    sem_t *semFree;
+    sem_t *semUsed;
+    sem_t *semGenerator;
+} sems_t;
+
 void printUsage(void);
-int parseArgs(int argc, char **argv);
-int setUpCircBuf(void);
+int parseArgs(int argc, char **argv, unsigned long *limit, unsigned long *delay);
+int setUpCircBuf(sems_t *sems);
 
 #endif //C_EXERCISES_SUPERVISOR_H
